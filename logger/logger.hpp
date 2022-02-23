2d0d76d7d006e20d0aac4c7d122d2dcd0ab5becb
Joseph <56293291+josephnglynn@users.noreply.github.com>
2022-02-23 21:48:25 +0000

    [Refactor] => Formatting

--- logger/logger.hpp
+++ logger/logger.hpp
@@ -353,9 +353,7 @@ namespace logger
 
 	inline void init(const bool use_std_out = true)
 	{
-		std::vector<OutputEntry> entries =
-			use_std_out ? std::vector<OutputEntry>({ internal::make_output_entry_with_cout() }) : std::vector<
-				OutputEntry>();
+		std::vector<OutputEntry> entries = use_std_out ? std::vector<OutputEntry>({ internal::make_output_entry_with_cout() }) : std::vector<OutputEntry>();
 		internal::logger_instance = std::make_unique<internal::Logger>(entries);
 	}
 
